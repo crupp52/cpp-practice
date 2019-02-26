86649ed86039f282ecebf27a2d2e77e75d6a6443
ZUCKLEVENTE-PC\ASUS <zuck.levente@protonmail.com>
2019-02-26 23:59:54 +0100

    First example is done.

--- cpp_practice_01/cpp_practice_01/cpp_practice_01.cpp
+++ cpp_practice_01/cpp_practice_01/cpp_practice_01.cpp
@@ -1,7 +1,40 @@
 #include "pch.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+bool isPrime(int num) {
+
+	for (int i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	srand(time(NULL));
+
+	int a[5];
+	for (int i = 0; i < 5; i++)
+	{
+		a[i] = rand() % 100 + 1;
+		//std::cout << a[i] << std::endl; 
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (isPrime(a[i]))
+		{
+			std::cout << a[i] << " prim" << std::endl;
+		}
+		else
+		{
+			std::cout << a[i] << " nem prim" << std::endl;
+		}
+	}
 }
+
